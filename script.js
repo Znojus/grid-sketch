3965974b7d27a22efc1186f15a675a704c302905
Znojus <150673971+Znojus@users.noreply.github.com>
2025-09-04 20:36:17 +0300

    Change the square divs's color to random rgb value
    To add:
    
    A button that would let the user select if he wants a random color
    or black.

--- script.js
+++ script.js
@@ -11,9 +11,15 @@ function createGrid(size) {
         horizDiv.style.flexDirection = "column";
 
         for(let j = 0; j < size; j++) {
+            let randomR = Math.floor(Math.random() * 256);
+            let randomG = Math.floor(Math.random() * 256);
+            let randomB = Math.floor(Math.random() * 256);
+
             let vertDiv = document.createElement("div");
             vertDiv.style.flex = "auto";
             vertDiv._opacity = 0;
+            vertDiv.style.opacity = 0;
+            vertDiv.style.backgroundColor = `rgb(${randomR}, ${randomB}, ${randomB})`;
             vertDiv.addEventListener("mouseenter", (event) => colorize(event.target));
             horizDiv.appendChild(vertDiv);
         }
@@ -23,7 +29,6 @@ function createGrid(size) {
 function colorize(element) {
     element._opacity += 0.1;
     element.style.opacity = element._opacity;
-    element.style.backgroundColor = "black";
 }
 
 function resize() {
